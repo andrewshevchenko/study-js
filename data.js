a479fd1f99ac43e8514119e9d6ddaf4104b3f02e
avshevchenko <avshevchenko@luxoft.com>
2016-07-13 15:08:38 +0300

    .

--- data.js
+++ data.js
@@ -4,15 +4,15 @@ var sourceData = {
         {
             name: "Group 1",
             items: [
-                {name: "item_1_1", img: "img/img_item_1_1.img", link: "google.com"},
-                {name: "item_1_2", img: "img/img_item_1_2.img", link: "yahoo.com"}
+                {name: "item_1_1", img: "img/img_item_1_1.png", link: "google.com"},
+                {name: "item_1_2", img: "img/img_item_1_2.png", link: "yahoo.com"}
             ]
         },
         {
             name: "Group 2",
             items: [
-                {name: "item_2_1", img: "img/img_item_2_1.img", link: "rambler.ru"},
-                {name: "item_2_2", img: "img/img_item_2_2.img", link: "ya.ru"}
+                {name: "item_2_1", img: "img/img_item_2_1.png", link: "rambler.ru"},
+                {name: "item_2_2", img: "img/img_item_2_2.png", link: "ya.ru"}
             ]
         }
     ]
